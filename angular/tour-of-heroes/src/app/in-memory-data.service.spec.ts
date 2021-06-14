5c6adf422f7a36df4b73713c43da8e563fe395ab
Nozomi Yonezawa <nozomi.yonezawa@nextbeat.net>
2021-06-14 23:28:24 +0900

    test for in memory data service

--- angular/tour-of-heroes/src/app/in-memory-data.service.spec.ts
+++ angular/tour-of-heroes/src/app/in-memory-data.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { InMemoryDataService } from './in-memory-data.service';
+import { Hero } from './hero';
 
 describe('InMemoryDataService', () => {
   let service: InMemoryDataService;
@@ -10,7 +11,19 @@ describe('InMemoryDataService', () => {
     service = TestBed.inject(InMemoryDataService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should return Hero Id', () => {
+    let hero: Hero[] = [{ id: 1, name: "sample Hero" }];
+    expect(service.genId(hero)).toEqual(2);
+
+    let heroesEmpty: Hero[] = [];
+    expect(service.genId(heroesEmpty)).toEqual(11);
+
+    let heroes: Hero[] = [
+      { id: 1, name: "sample Hero" },
+      { id: 2, name: "sample Hero" },
+      { id: 3, name: "sample Hero" },
+      { id: 4, name: "sample Hero" },
+    ];
+    expect(service.genId(heroes)).toEqual(5);
   });
 });
